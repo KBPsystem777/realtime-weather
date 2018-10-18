e1c4936fe519a5703a9b9de36570935dc19dd6b5
kbpsystem777 <koleen.bp@outlook.com>
2018-10-19 02:08:27 +0800

    using different api provider

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,68 +3,32 @@ import React from 'react';
 const axios = require('axios');
 const key = process.env.key;
 
-require('dotenv').config({path: './key.env'});
+require('dotenv').config({
+    path: './key.env'
+});
 
 class GetWeather extends React.Component {
-
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            latitude: '',
-            longitude: '',
-            temparature: ''
-        }
-
-        this.getLocation = this.getLocation.bind(this);
-    }
-
-    
-
-    componentDidMount() {
-        this.getLocation()
-    }
-
-
-
-    getLocation() {
-        const location = window.navigator && navigator.geolocation
-        if(location) {               
-            location.getCurrentPosition((position) => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                })
-            
-                const endpoint = `https://api.darksky.net/forecast/${key}/${this.state.latitude},${this.state.longitude}`
-                axios.get(endpoint)
-                .then((json) => {
-                    const returnData = json.data.currently.temperature;
-                    console.log(returnData)
-                    return returnData
-                })
-            
-            
-            
-            }, (error) => {
-                this.setState({ latitude: 'err-latitude', longitude: 'err-longitude'})
-            })
+            city: '',
+            country: '',
+            temperature: ''
         }
     }
 
-    
+    getWeatherAPI = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.city.value;
+    }
 
     render() {
-        const { latitude, longitude, temparature } = this.state;
+        <div>
 
-        return(
-            <div>
-                <div>Your Location Info:</div>
-                <p>Latitude: {latitude}</p>
-                <p>Longitude: {longitude}</p>
-                <p>Temparature: {temparature}</p>
-            </div>
-        )
+        </div>
     }
 }
 
+    
 export default GetWeather;
